d8965ff2e2d7d8af2528147b5eec7fde0e04124d
Diogo Azevedo <idiogoazevedoo@gmail.com>
2024-04-21 19:17:50 +0100

    e2e: add and config playwright for e2e tests, add sign-in e2e tests

---
+++ test/sign-in.e2e-spec.ts
@@ -0,0 +1,31 @@
+import { expect, test } from '@playwright/test'
+
+test('sign in successfully', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Your e-mail').fill('johndoe@example.com')
+  await page.getByRole('button', { name: 'Access Panel' }).click()
+
+  const toast = page.getByText('Auth link sent to your e-mail.')
+
+  expect(toast).toBeVisible()
+})
+
+test('sign in with wrong credentials', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByLabel('Your e-mail').fill('wrong@example.com')
+  await page.getByRole('button', { name: 'Access Panel' }).click()
+
+  const toast = page.getByText('Invalid credentials.')
+
+  expect(toast).toBeVisible()
+})
+
+test('navigate to new restaurant page', async ({ page }) => {
+  await page.goto('/sign-in', { waitUntil: 'networkidle' })
+
+  await page.getByRole('link', { name: 'New partner' }).click()
+
+  expect(page.url()).toContain('/sign-up')
+})
